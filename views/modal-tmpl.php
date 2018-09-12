34db90898fe6cc710ccd9f2f06597ab67b89d17f
Eric Fuller <info@glassmoonmedia.com>
2018-09-11 21:29:40 -0400

    Adjust template markup.

--- views/modal-tmpl.php
+++ views/modal-tmpl.php
@@ -14,11 +14,11 @@
 
 	</article>
 	<# if ( data.next_article ) { #>
-	<a class="next-link next" href="{{data.next_article}}" data-id="{{data.next_id}}">
-		><span class="screen-reader-text">Next Article</span></a>
+	<a class="next-link previous" href="{{data.next_article}}" data-id="{{data.next_id}}">
+		<<span class="screen-reader-text">Next Article</span></a>
 	<# } #>
 	<# if ( data.previous_article ) { #>
-	<a class="next-link previous" href="{{data.previous_article}}" data-id="{{data.previous_id}}"><<span
-				class="screen-reader-text">Previous Article</span></a>
+	<a class="next-link next" href="{{data.previous_article}}" data-id="{{data.previous_id}}">
+		><span class="screen-reader-text">Previous Article</span></a>
 	<# } #>
 </script>
